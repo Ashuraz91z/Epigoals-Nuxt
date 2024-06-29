07029930026d38a0dbeaeb47b96118e393708863
Lucas Fernandes <lucas.epitech@epitech.digital>
2024-06-29 17:02:32 +0200

    Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    pageTransition: { name: "fade", mode: "out-in" },
+  },
+
+  modules: ["@nuxtjs/tailwindcss"],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class", // ou 'media'
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.{js,vue,ts}",
+    "./pages/**/*.{js,vue,ts}",
+    "./plugins/**/*.{js,vue,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
